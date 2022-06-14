3a471756ff902fc4ddc967007940820fd89920ea
David Escobar <david.escobar@unholster.com>
2022-06-14 17:04:07 -0400

    ?

--- broker.js
+++ broker.js
@@ -10,24 +10,9 @@ server.listen(port, function () {
     aedes.publish({topic: '$queue/device/sck/+/+', payload: 'hola'})
 })
 
-aedes.on('subscribe', function (subscriptions, client) {
-    console.log('MQTT client \x1b[32m' + (client ? client.id : client) +
-        '\x1b[0m subscribed to topics: ' + subscriptions.map(s => s.topic).join('\n'), 'from broker', aedes.id)
-})
-
-aedes.on('unsubscribe', function (subscriptions, client) {
-    console.log('MQTT client \x1b[32m' + (client ? client.id : client) +
-        '\x1b[0m unsubscribed to topics: ' + subscriptions.join('\n'), 'from broker', aedes.id)
-})
-
 // fired when a client connects
 aedes.on('client', function (client) {
     console.log('Client Connected: \x1b[33m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
-})
-
-// fired when a client disconnects
-aedes.on('clientDisconnect', function (client) {
-    console.log('Client Disconnected: \x1b[31m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
 })
 
 // fired when a message is published
